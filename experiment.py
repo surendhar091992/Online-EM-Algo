3bde2e0bbd6b5813015993534565dd52421e842c
Leon Zheng <leonpapanoel@gmail.com>
2019-12-09 22:03:17 +0100

    Changing experience. Basic mixture poisson ok. Need to plot results

--- experiment.py
+++ experiment.py
@@ -10,28 +10,38 @@ import numpy as np
 Generating a problem instance
 """
 # Parameters
-n = 100  # Size of the data set
+n = 1000  # Size of the data set
 m = 3 # Number of clusters
-max_l = 10 # Maximum possible value for parameter lambda of Poisson
+max_l = 100 # Maximum possible value for parameter lambda of Poisson
 
-# Random parameters
-l, p = poisson_random_param(m, max_l) # Ground truth
-theta_true = np.array([p, l])
+# # Random parameters
+# theta_true = poisson_random_param(m, max_l) # Ground truth
+
+# Choosen parameters
+p_true = np.ones(m) / m
+l_true = [1, 10, 100]
+theta_true = np.array([p_true, l_true])
 
 # Data set
-Y, W = sample_poisson(n, l, p)
+Y, W = sample_poisson(n, theta_true[1], theta_true[0])
 
 """
 Online EM algo
 """
 # Parameters
 gamma_0 = 1
-alpha = 1.
-gamma = np.array([gamma_0 * np.power(n, -alpha) for l in range(1, n+1)])
+alpha = 0.6
+gamma = np.array([gamma_0 * np.power(l, -alpha) for l in range(1, n+1)])
 
-# Initialization
-p_init, l_init = poisson_random_param(m, max_l)
-theta_init = np.array([p_init, l_init])
+# Random initialization
+l, p = poisson_random_param(m, max_l)
+theta_init = np.array([l, p])
+
+# # Choosen initialization
+# p_init = [0.2, 0.35, 0.45]
+# l_init = [40, 20, 70]
+# theta_init = np.array([p_init, l_init])
+
 print(f"Initial Theta:\n{theta_init}")
 
 # Online EM algo
--- online_EM.py
+++ online_EM.py
@@ -2,28 +2,31 @@
 Implementation of Online EM Algorithm
 Leon Zheng
 """
+
 import numpy as np
 
 def online_EM_iter(new_data, s, theta, new_gamma, s_bar, theta_bar):
     new_s = s + new_gamma * (s_bar(new_data, theta) - s)
-    print(f'new_s = {new_s}')
+    # print(f'new_s = {new_s}')
     new_theta = theta_bar(new_s)
-    print(f'new_theta = {new_theta}')
+    # print(f'new_theta = {new_theta}')
     return new_s, new_theta
 
 
 def online_EM(data, init, gamma, s_bar, theta_bar):
-    s = 0
     theta = init
+    s = 0
     iter = len(data)
     for i in range(iter):
+        print(f'gamma = {gamma[i]}, Y_new = {data[i]}')
         s, theta = online_EM_iter(data[i], s, theta, gamma[i], s_bar, theta_bar)
+        print([s[j,1]/s[j,0] for j in range(s.shape[0])])
     return s, theta
 
 # online_em2 (Titterington)
 from mixture_poisson import poisson_theta_bar2
 def online_EM2(data, init, gamma):
-    theta =init
+    theta = init
     iter = len(data)
     for i in range(iter):
         theta = poisson_theta_bar2(data[i],theta,gamma[i])
